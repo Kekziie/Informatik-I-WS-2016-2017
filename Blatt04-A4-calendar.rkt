4df6b4112c3e9b40e3b84113eb05a8776ad98762
Kim Thuong Ngo <kimthuongngo3@gmail.com>
2016-11-14 22:12:31 +0100

    add task b c

--- Blatt04-A4-calendar.rkt
+++ Blatt04-A4-calendar.rkt
@@ -17,3 +17,66 @@
   (calendar-date-day
    calendar-date-month
    calendar-date-year))
+
+; (b)
+; stellt fest, ob Monat korrekt ist
+
+(define calendar-date-month-ok?
+  (lambda (date)
+    (if (>= (calendar-date-month date) 1)
+        (<= (calendar-date-month date) 12)
+        #f)))
+
+; stellt fest, ob Tage im Monat korrekt sind
+
+(define calendar-date-day-ok?
+  (lambda (date)
+    (if (>= (calendar-date-day date) 1)
+        (if (<= (calendar-date-month date) 7)
+            (if (= (modulo (calendar-date-month date) 2) 1)
+                (<= (calendar-date-day date) 31)
+                (if (= (calendar-date-month date) 2)
+                    (<= (calendar-date-day date) 28)
+                    (<= (calendar-date-day date) 30)))
+            (if (= (modulo (calendar-date-month date) 2) 0)
+                (<= (calendar-date-day date) 31)
+                (<= (calendar-date-day date) 30)))
+        #f)))
+
+; stellt fest, ob Kalenderdatum-Record einem tatsächlichen Kalenderdatum entspricht
+; Schaltjahre ignoriert
+
+(define calendar-date-ok?
+  (lambda (date)
+    (and (calendar-date-month-ok? date)
+         (calendar-date-day-ok? date))))
+
+; (c)
+; stellt fest, ob Tage im Monat korrekt sind im Schaltjahr
+
+(define calendar-date-day-ok/leap-year?
+  (lambda (date)
+    (if (>= (calendar-date-day date) 1)
+        (if (<= (calendar-date-month date) 7)
+            (if (= (modulo (calendar-date-month date) 2) 1)
+                (<= (calendar-date-day date) 31)
+                (if (= (calendar-date-month date) 2)
+                    (<= (calendar-date-day date) 29)
+                    (<= (calendar-date-day date) 30)))
+            (if (= (modulo (calendar-date-month date) 2) 0)
+                (<= (calendar-date-day date) 31)
+                (<= (calendar-date-day date) 30)))
+        #f)))
+
+; stellt fest, ob Kalenderdatum-Record einem tatsächlichen Kalenderdatum entspricht
+; mit Schaltjahre 
+
+(define calendar-date-ok/leap-year?
+  (lambda (date)
+    (if (= (modulo (calendar-date-year date) 4) 0)
+        (and (calendar-date-day-ok/leap-year? date)
+             (calendar-date-month-ok? date))
+        (calendar-date-ok? date)))) 
+
+
+     
