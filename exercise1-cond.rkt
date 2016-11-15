d3ee040fec63ee779c6cc4f8f17124d8cb02f75d
Kim Thuong Ngo <kimthuongngo3@gmail.com>
2016-11-15 17:36:45 +0100

    add exercise 1

---
+++ exercise1-cond.rkt
@@ -0,0 +1,6 @@
+;; Die ersten drei Zeilen dieser Datei wurden von DrRacket eingefügt. Sie enthalten Metadaten
+;; über die Sprachebene dieser Datei in einer Form, die DrRacket verarbeiten kann.
+#reader(lib "DMdA-beginner-reader.ss" "deinprogramm")((modname exercise1-cond) (read-case-sensitive #f) (teachpacks ((lib "image2.rkt" "teachpack" "deinprogramm") (lib "universe.rkt" "teachpack" "deinprogramm"))) (deinprogramm-settings #(#f write repeating-decimal #f #t none explicit #f ((lib "image2.rkt" "teachpack" "deinprogramm") (lib "universe.rkt" "teachpack" "deinprogramm")))))
+; Bewertungsschema einer Klausur
+; jeder Punktzahl soll einer Note zugeordnet werden
+; mind. 90 Punkte für eine 1, alle 10 Punkte darunter ein Note tiefer
