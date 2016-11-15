7622f3c7672b89f42ed905fff6f3079be3a55c79
Kim Thuong Ngo <kimthuongngo3@gmail.com>
2016-11-15 17:56:24 +0100

    add task

---
+++ exercise2-image-chessboard.rkt
@@ -0,0 +1,5 @@
+;; Die ersten drei Zeilen dieser Datei wurden von DrRacket eingefügt. Sie enthalten Metadaten
+;; über die Sprachebene dieser Datei in einer Form, die DrRacket verarbeiten kann.
+#reader(lib "DMdA-beginner-reader.ss" "deinprogramm")((modname exercise2-image-chessboard) (read-case-sensitive #f) (teachpacks ((lib "image2.rkt" "teachpack" "deinprogramm") (lib "universe.rkt" "teachpack" "deinprogramm"))) (deinprogramm-settings #(#f write repeating-decimal #f #t none explicit #f ((lib "image2.rkt" "teachpack" "deinprogramm") (lib "universe.rkt" "teachpack" "deinprogramm")))))
+; rekonstruieren eines Schachbretts mit Figuren
+; bewegen eines Bauern 2 Felder vorwärts
