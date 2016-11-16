ac38875bcb423f8e87e0dc4635d5d6c28ec68125
Kim Thuong Ngo <kimthuongngo3@gmail.com>
2016-11-16 12:36:18 +0100

    add task of record-procedure

---
+++ exercise3-Record-Procedures.rkt
@@ -0,0 +1,29 @@
+;; Die ersten drei Zeilen dieser Datei wurden von DrRacket eingefügt. Sie enthalten Metadaten
+;; über die Sprachebene dieser Datei in einer Form, die DrRacket verarbeiten kann.
+#reader(lib "DMdA-beginner-reader.ss" "deinprogramm")((modname exercise3-Record-Procedures) (read-case-sensitive #f) (teachpacks ((lib "image2.rkt" "teachpack" "deinprogramm") (lib "universe.rkt" "teachpack" "deinprogramm"))) (deinprogramm-settings #(#f write repeating-decimal #f #t none explicit #f ((lib "image2.rkt" "teachpack" "deinprogramm") (lib "universe.rkt" "teachpack" "deinprogramm")))))
+; Record-Procedure über das Handyspiel "One Piece: Treasure Cruise"
+
+; Record-Procedure über die Charaktere im Spiel
+; Ein Charakter (character) besteht aus:
+; - ID
+; - name
+; - type
+; - class1
+; - class2
+; - rarity
+; - special
+; - evolve-to
+; - powers
+
+(define-record-procedures character
+  make-character
+  character?
+  (character-ID
+   character-name
+   character-type
+   character-class1
+   character-class2
+   character-rarity
+   character-special
+   character-evolve-to
+   character-powers))
