c836d2955a1eca9e5d00954baee00200bd617a8b
Kim Thuong Ngo <kimthuongngo3@gmail.com>
2016-11-17 12:02:57 +0100

    add definition of signature

--- exercise3-Record-Procedures.rkt
+++ exercise3-Record-Procedures.rkt
@@ -3,10 +3,12 @@
 #reader(lib "DMdA-beginner-reader.ss" "deinprogramm")((modname exercise3-Record-Procedures) (read-case-sensitive #f) (teachpacks ((lib "image2.rkt" "teachpack" "deinprogramm") (lib "universe.rkt" "teachpack" "deinprogramm"))) (deinprogramm-settings #(#f write repeating-decimal #f #t none explicit #f ((lib "image2.rkt" "teachpack" "deinprogramm") (lib "universe.rkt" "teachpack" "deinprogramm")))))
 ; Record-Procedure über das Handyspiel "One Piece: Treasure Cruise"
 
+; definieren aller Eigenschaften eines Charakters
+
 ; definiere character-type in signature
 ; type "all" includes STR DEX QCK INT and PSY
 
-(define type
+(define char-type
   (signature (one-of "STR"
                      "DEX"
                      "QCK"
@@ -34,6 +36,36 @@
 (define rarity
   (signature (one-of 1 2 3 4 5 6)))
 
+; evolve-to soll auf die ID des Charakters hinweisen, zu dem er sich entwickeln kann
+
+; definiere power-type für signature
+
+(define pow-type
+  (signature (one-of "Charge Special"
+                     "Bind Resistance"
+                     "Poison Resistance"
+                     "Map Damage Resistance"
+                     "Resilience"
+                     "RCV Boost"
+                     "none")))
+
+; Record-Procedure über character-powers
+; eine character-power besteht aus
+; - count
+;   -> count=0 means character have no powers
+; - type
+
+(: make-powers (natural pow-type -> powers))
+
+(define-record-procedures powers
+  make-powers
+  powers?
+   (powers-count
+    powers-tye))
+
+(define powers-ID1
+  (make-powers 0 "none"))
+
 ; Record-Procedure über die Charaktere im Spiel
 ; Ein Charakter (character) besteht aus:
 ; - name
@@ -41,12 +73,13 @@
 ; - class1
 ; - class2
 ; - rarity
-; - special-level-current
+; - special-level-current 
 ; - special-level-max
+;   -> special-level = 0 means this character doesn't own a special
 ; - evolve-to
 ; - powers
 
-(: make-character (string type classes classes natural natural natural string natural -> character))
+(: make-character (string char-type classes classes rarity natural natural string natural -> character))
 
 (define-record-procedures character
   make-character
@@ -65,19 +98,4 @@
 
 (define ID1
   (make-character "Luffy" "STR" "Fighter" "none" 2 0 1 "ID2" 0))
-
-; Record-Procedure über character-powers
-; eine character-power besteht aus
-; - ID
-; - count
-; - type
-
-(: make-powers (natural string -> powers))
-
-(define-record-procedures powers
-  make-powers
-  powers?
-   (powers-count
-    powers-tye))
-
 
