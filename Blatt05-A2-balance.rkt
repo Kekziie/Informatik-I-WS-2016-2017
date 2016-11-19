d9ae1d3b3f3382e811b2b1c2e070b6f916314d5d
Kim Thuong Ngo <kimthuongngo3@gmail.com>
2016-11-19 13:15:40 +0100

    add task2

---
+++ Blatt05-A2-balance.rkt
@@ -0,0 +1,8 @@
+;; Die ersten drei Zeilen dieser Datei wurden von DrRacket eingefügt. Sie enthalten Metadaten
+;; über die Sprachebene dieser Datei in einer Form, die DrRacket verarbeiten kann.
+#reader(lib "DMdA-beginner-reader.ss" "deinprogramm")((modname Blatt05-A2-balance) (read-case-sensitive #f) (teachpacks ((lib "image2.rkt" "teachpack" "deinprogramm") (lib "universe.rkt" "teachpack" "deinprogramm"))) (deinprogramm-settings #(#f write repeating-decimal #f #t none explicit #f ((lib "image2.rkt" "teachpack" "deinprogramm") (lib "universe.rkt" "teachpack" "deinprogramm")))))
+; Aufgabe 2
+
+; vergleichen von Flächen unterschiedlicher geometrischer Formen (shapes) mit Hilfe einer "Waage"
+; dabei soll der Flächeninhalt als "Gewicht" dienen
+
