ab638882d1da6eaad3708488759db49c9ee8681c
Kim Thuong Ngo <kimthuongngo3@gmail.com>
2016-11-19 13:24:42 +0100

    add function "Wochentag-index"

--- Blatt05-A3-dayofweek.rkt
+++ Blatt05-A3-dayofweek.rkt
@@ -5,3 +5,27 @@
 
 ; ersetzen, der in der Signatur vorkommende (one-of ...) in andere gleichwertige Konstrukte
 
+(define Mo "Montag")
+(define Di "Dienstag")
+(define Mi "Mittwoch")
+(define Do "Donnerstag")
+(define Fr "Freitag")
+(define Sa "Samstag")
+(define So "Sonntag")
+
+; ermittelt die Nummer eines Wochentages innerhalb einer Woche
+; die Nummerierung beginnt mit 0 und nimmt Montag als ersten Tag der Woche an
+
+(: wochentag-index ((one-of Mo Di Mi Do Fr Sa So) -> (one-of 0 1 2 3 4 5 6)))
+
+(define wochentag-index
+  (lambda (tag)
+    (cond
+      ((string=? tag Mo) 0)
+      ((string=? tag Di) 1)
+      ((string=? tag Mi) 2)
+      ((string=? tag Do) 3)
+      ((string=? tag Fr) 4)
+      ((string=? tag Sa) 5)
+      ((string=? tag So) 6)
+      )))
