ad742d0f35029da4ad49297d073b0be26212dbe0
Kim Thuong Ngo <ngoki@web.de>
2016-12-03 21:59:06 +0100

    add procedure

--- Blatt06-A2-list-functions.rkt
+++ Blatt06-A2-list-functions.rkt
@@ -1,6 +1,6 @@
 ;; Die ersten drei Zeilen dieser Datei wurden von DrRacket eingefügt. Sie enthalten Metadaten
 ;; über die Sprachebene dieser Datei in einer Form, die DrRacket verarbeiten kann.
-#reader(lib "DMdA-vanilla-reader.ss" "deinprogramm")((modname Blatt06-A2-list-functions) (read-case-sensitive #f) (teachpacks ()) (deinprogramm-settings #(#f write repeating-decimal #f #t none explicit #f ())))
+#reader(lib "DMdA-beginner-reader.ss" "deinprogramm")((modname Blatt06-A2-list-functions) (read-case-sensitive #f) (teachpacks ()) (deinprogramm-settings #(#f write repeating-decimal #f #t none explicit #f ())))
 ; Aufgabe 2
 
 ; (a)
@@ -57,6 +57,13 @@
                                (make-pair 0
                                           (make-pair 1
                                                     empty)))) 1)
+
+(define last
+  (lambda (xs)
+    (cond
+      ((empty? xs) (violation "Liste ist leer"))
+      ((empty? (rest xs)) (first xs))
+      ((pair? xs) (last (rest xs))))))
 
 
 
