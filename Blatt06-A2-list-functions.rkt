dc66fe34dd3620870d0a274edec0aa331e3055e7
Kim Thuong Ngo <ngoki@web.de>
2016-12-03 21:55:03 +0100

    add file w06 A1

---
+++ Blatt06-A2-list-functions.rkt
@@ -0,0 +1,4 @@
+;; Die ersten drei Zeilen dieser Datei wurden von DrRacket eingefügt. Sie enthalten Metadaten
+;; über die Sprachebene dieser Datei in einer Form, die DrRacket verarbeiten kann.
+#reader(lib "DMdA-vanilla-reader.ss" "deinprogramm")((modname Blatt06-A2-list-functions) (read-case-sensitive #f) (teachpacks ()) (deinprogramm-settings #(#f write repeating-decimal #f #t none explicit #f ())))
+; Aufgabe 2
