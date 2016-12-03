1fc38b024fdefc73106100301e5236925eb90224
Kim Thuong Ngo <ngoki@web.de>
2016-12-03 21:47:02 +0100

    add function f

--- Blatt06-A1-let.rkt
+++ Blatt06-A1-let.rkt
@@ -2,3 +2,9 @@
 ;; über die Sprachebene dieser Datei in einer Form, die DrRacket verarbeiten kann.
 #reader(lib "DMdA-beginner-reader.ss" "deinprogramm")((modname Blatt06-A1-let) (read-case-sensitive #f) (teachpacks ()) (deinprogramm-settings #(#f write repeating-decimal #f #t none explicit #f ())))
 ; Aufgabe 1
+
+; Funktionen g,h und i beliebig
+
+;(define f
+;  (lambda (x)
+;    (i (g x) (h (g x)))))  
