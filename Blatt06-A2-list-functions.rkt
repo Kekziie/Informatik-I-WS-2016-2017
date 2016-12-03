22aaa4698427331dd2fa7cd1a3d2c142e51ee068
Kim Thuong Ngo <ngoki@web.de>
2016-12-03 21:55:53 +0100

    add definition of script

--- Blatt06-A2-list-functions.rkt
+++ Blatt06-A2-list-functions.rkt
@@ -2,3 +2,40 @@
 ;; über die Sprachebene dieser Datei in einer Form, die DrRacket verarbeiten kann.
 #reader(lib "DMdA-vanilla-reader.ss" "deinprogramm")((modname Blatt06-A2-list-functions) (read-case-sensitive #f) (teachpacks ()) (deinprogramm-settings #(#f write repeating-decimal #f #t none explicit #f ())))
 ; Aufgabe 2
+
+; (a)
+; Daten- und Recorddefinition für polymorphe Paare
+; ein polymorphes Paar (pair) besteht aus
+; - erste Komponente (first)
+; - zweite Komponente (rest)
+; Hinweis: Komponenten können beliebige Werte annehmen
+(define-record-procedures-parametric pair pair-of
+  make-pair
+  pair?
+  (first
+   rest))
+
+; Datendefinitionen
+(define Liste123
+  (make-pair 1
+             (make-pair 2
+                        (make-pair 3
+                                   empty))))
+(define Wochenende
+  (make-pair "Freitag"
+             (make-pair "Samstag"
+                        (make-pair "Sonntag"
+                                   empty))))
+(define Wahrheitswerte
+  (make-pair #t
+             #f))
+
+; Signatur von list-of
+; eine Liste kann:
+;  - leer sein (empty-list)
+;  - aus einem Paar (pair-of) bestehen (siehe oben)
+(define list-of
+  (lambda (t)
+    (signature (mixed empty-list
+                      (pair-of t (list-of t))
+                      ))))
