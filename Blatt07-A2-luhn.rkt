682c37080a5e0958b1a64b712d310b21e01cdb88
Kim Thuong Ngo <kimthuongngo3@gmail.com>
2016-12-04 13:15:46 +0100

    add method of "luan check"

--- Blatt07-A2-luhn.rkt
+++ Blatt07-A2-luhn.rkt
@@ -1,4 +1,11 @@
 ;; Die ersten drei Zeilen dieser Datei wurden von DrRacket eingefügt. Sie enthalten Metadaten
 ;; über die Sprachebene dieser Datei in einer Form, die DrRacket verarbeiten kann.
 #reader(lib "DMdA-vanilla-reader.ss" "deinprogramm")((modname Blatt07-A2-luhn) (read-case-sensitive #f) (teachpacks ((lib "image2.rkt" "teachpack" "deinprogramm") (lib "universe.rkt" "teachpack" "deinprogramm"))) (deinprogramm-settings #(#f write repeating-decimal #f #t none explicit #f ((lib "image2.rkt" "teachpack" "deinprogramm") (lib "universe.rkt" "teachpack" "deinprogramm")))))
-; Aufgabe 2
+; Aufgabe 2 Luhn-Check
+; Verfahren definiert für einer Zahl n
+; i) gehe die Ziffern von n, rechts nach links durch, verdopple jede 2.Ziffer
+; ii) falls Verdopplung 2 Ziffern hat -> Aufspaltung in 2 unabhängig voneinander zu betrachtenden Ziffern
+; iii) Summe aus allen Ziffern
+; iv) dividiere durch 10
+; v) Rest der Division 0 -> gültige Zahl n, Luhn-Check bestanden
+;    Rest ungleich 0 -> ungültige Zahl n, Luhn-Check nicht bestanden
