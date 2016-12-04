248e29049f1a7b3c15a242f6bae16c4aabc103ac
Kim Thuong Ngo <kimthuongngo3@gmail.com>
2016-12-04 12:07:25 +0100

    add new file

---
+++ Blatt06-A3-insertion-sort.rkt
@@ -0,0 +1,4 @@
+;; Die ersten drei Zeilen dieser Datei wurden von DrRacket eingefügt. Sie enthalten Metadaten
+;; über die Sprachebene dieser Datei in einer Form, die DrRacket verarbeiten kann.
+#reader(lib "DMdA-beginner-reader.ss" "deinprogramm")((modname Blatt06-A3-insertion-sort) (read-case-sensitive #f) (teachpacks ((lib "image2.rkt" "teachpack" "deinprogramm") (lib "universe.rkt" "teachpack" "deinprogramm"))) (deinprogramm-settings #(#f write repeating-decimal #f #t none explicit #f ((lib "image2.rkt" "teachpack" "deinprogramm") (lib "universe.rkt" "teachpack" "deinprogramm")))))
+; Aufgabe 3 
