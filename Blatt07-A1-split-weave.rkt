7b0b7960229041d83482ed14a1e54c13db56351b
Kim Thuong Ngo <kimthuongngo3@gmail.com>
2016-12-04 12:20:19 +0100

    add task 1a)

--- Blatt07-A1-split-weave.rkt
+++ Blatt07-A1-split-weave.rkt
@@ -2,3 +2,12 @@
 ;; über die Sprachebene dieser Datei in einer Form, die DrRacket verarbeiten kann.
 #reader(lib "DMdA-vanilla-reader.ss" "deinprogramm")((modname Blatt07-A1-split-weave) (read-case-sensitive #f) (teachpacks ((lib "image2.rkt" "teachpack" "deinprogramm") (lib "universe.rkt" "teachpack" "deinprogramm"))) (deinprogramm-settings #(#f write repeating-decimal #f #t none explicit #f ((lib "image2.rkt" "teachpack" "deinprogramm") (lib "universe.rkt" "teachpack" "deinprogramm")))))
 ; Aufgabe 1
+
+; (a)
+; schreiben einer parametrisch polymorphen Record-Typ für tuple
+
+; Prozedur "split-list" soll eine Liste in zwei Listen aufspalten
+; die Elemente sollen abwechselnd auf beide Ergebnislisten aufgeteilt werden
+; bei ungerader Azahl in Eingabeliste -> 1. Ergebnisliste länger
+
+(: split-list ((list-of %a) -> (tuple-of (list-of %a) (list-of %a))))
