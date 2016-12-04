c938ac1b0a45c325d4bef44053374ecb259bf814
Kim Thuong Ngo <kimthuongngo3@gmail.com>
2016-12-04 12:08:25 +0100

    add definition of script

--- Blatt06-A3-insertion-sort.rkt
+++ Blatt06-A3-insertion-sort.rkt
@@ -1,4 +1,38 @@
 ;; Die ersten drei Zeilen dieser Datei wurden von DrRacket eingefügt. Sie enthalten Metadaten
 ;; über die Sprachebene dieser Datei in einer Form, die DrRacket verarbeiten kann.
 #reader(lib "DMdA-beginner-reader.ss" "deinprogramm")((modname Blatt06-A3-insertion-sort) (read-case-sensitive #f) (teachpacks ((lib "image2.rkt" "teachpack" "deinprogramm") (lib "universe.rkt" "teachpack" "deinprogramm"))) (deinprogramm-settings #(#f write repeating-decimal #f #t none explicit #f ((lib "image2.rkt" "teachpack" "deinprogramm") (lib "universe.rkt" "teachpack" "deinprogramm")))))
-; Aufgabe 3 
+; Aufgabe 3
+
+; Daten- und Recorddefinition für polymorphe Paare
+; ein polymorphes Paar (pair) besteht aus
+; - erste Komponente (first)
+; - zweite Komponente (rest)
+; Hinweis: Komponenten können beliebige Werte annehmen
+(define-record-procedures-parametric pair pair-of
+  make-pair
+  pair?
+  (first
+   rest))
+
+; Datendefinitionen
+(define Liste123
+  (make-pair 1
+             (make-pair 2
+                        (make-pair 3
+                                   empty))))
+(define Liste1
+  (make-pair 3
+             (make-pair 12
+                        (make-pair 45
+                                   (make-pair 113
+                                              empty)))))
+
+; Signatur von list-of
+; eine Liste kann:
+;  - leer sein (empty-list)
+;  - aus einem Paar (pair-of) bestehen (siehe oben)
+(define list-of
+  (lambda (t)
+    (signature (mixed empty-list
+                      (pair-of t (list-of t))
+                      ))))
