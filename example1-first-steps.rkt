46a7b18bdbaf6b8fc979c7e0ff59d169f3497a7d
Kim Thuong Ngo <kimthuongngo3@gmail.com>
2016-12-06 23:05:52 +0100

    add examples 1

---
+++ example1-first-steps.rkt
@@ -0,0 +1,33 @@
+;; Die ersten drei Zeilen dieser Datei wurden von DrRacket eingefügt. Sie enthalten Metadaten
+;; über die Sprachebene dieser Datei in einer Form, die DrRacket verarbeiten kann.
+#reader(lib "DMdA-vanilla-reader.ss" "deinprogramm")((modname example1-first-steps) (read-case-sensitive #f) (teachpacks ((lib "image2.rkt" "teachpack" "deinprogramm") (lib "universe.rkt" "teachpack" "deinprogramm"))) (deinprogramm-settings #(#f write repeating-decimal #f #t none explicit #f ((lib "image2.rkt" "teachpack" "deinprogramm") (lib "universe.rkt" "teachpack" "deinprogramm")))))
+; Beispiele 1
+
+; i) Bindung von Namen (identifier) an Werten
+;    (define {id} {expression})
+
+(define pi 3.1415)
+(define absoluter-nullpunkt -273.15)
+(define minutes-in-a-day (* 24 60))
+(define Uni "Universität")
+(define Quadrat1 (square 25 "solid" "black"))
+
+; ii) schreiben von "kleinen" Prozeduren nach Konstruktionsanleitung
+; Konstruktionsanleitung: 1) Kurzbeschreibung
+;                         2) Signatur
+;                         3) Testfälle
+;                         4) Prozedur
+
+; Um wieviele Menschen wächst die Weltbevölkerung in Tagen (days)?
+(: population-growth-in-days (natural -> natural))
+
+(check-expect (population-growth-in-days 0) 0)
+(check-expect (population-growth-in-days 1) 223200)
+(check-expect (population-growth-in-days 5) 1116000)
+(check-expect (population-growth-in-days 10) 2232000)
+
+(define population-growth-in-days
+  (lambda (days) (* days (* 155 minutes-in-a-day))))
+
+
+
