cff9ccfff5421aee2d814252af5249cf0b91c001
Kim Thuong Ngo <kimthuongngo3@gmail.com>
2016-12-06 23:26:01 +0100

    add record-procedure

--- exercise5-vectors.rkt
+++ exercise5-vectors.rkt
@@ -2,3 +2,24 @@
 ;; über die Sprachebene dieser Datei in einer Form, die DrRacket verarbeiten kann.
 #reader(lib "DMdA-vanilla-reader.ss" "deinprogramm")((modname exercise5-vectors) (read-case-sensitive #f) (teachpacks ((lib "image2.rkt" "teachpack" "deinprogramm") (lib "universe.rkt" "teachpack" "deinprogramm"))) (deinprogramm-settings #(#f write repeating-decimal #f #t none explicit #f ((lib "image2.rkt" "teachpack" "deinprogramm") (lib "universe.rkt" "teachpack" "deinprogramm")))))
 ; Übung 5 Vektoren
+
+; Record- und Datendefinition
+
+; ein Vektor (vector) besteht aus
+; - x-Koordinate
+; - y-Koordinate
+; - z-Koordinate
+
+(: make-vector (real real real -> vector))
+(: vector-x (vector -> real))
+(: vector-y (vector -> real))
+(: vector-z (vector -> real))
+(: vector? (any -> boolean))
+
+(define-record-procedures vector
+  make-vector
+  vector?
+  (vector-x
+   vector-y
+   vector-z))
+  
