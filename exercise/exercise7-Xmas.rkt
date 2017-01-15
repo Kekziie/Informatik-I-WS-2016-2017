878c625ce887496dd72969cb8596fd6d6ca6cab6
Kim <Kim@Kims-MacBook-Pro.local>
2017-01-15 22:41:03 +0100

    Delete

--- exercise/exercise7-Xmas.rkt
+++
@@ -1,4 +0,0 @@
-;; Die ersten drei Zeilen dieser Datei wurden von DrRacket eingefügt. Sie enthalten Metadaten
-;; über die Sprachebene dieser Datei in einer Form, die DrRacket verarbeiten kann.
-#reader(lib "DMdA-beginner-reader.ss" "deinprogramm")((modname exercise7-Xmas) (read-case-sensitive #f) (teachpacks ((lib "image2.ss" "teachpack" "deinprogramm") (lib "universe.rkt" "teachpack" "deinprogramm"))) (deinprogramm-settings #(#f write repeating-decimal #f #t none explicit #f ((lib "image2.ss" "teachpack" "deinprogramm") (lib "universe.rkt" "teachpack" "deinprogramm")))))
-; Übung 7 Christmas
