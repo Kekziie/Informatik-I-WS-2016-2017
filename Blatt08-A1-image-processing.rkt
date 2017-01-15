65c40b450fe77e1ba814bb02e84f20f8a726044a
Kekziie <Kim@Kims-MacBook-Pro.local>
2017-01-15 20:04:07 +0100

    Add task

--- Blatt08-A1-image-processing.rkt
+++ Blatt08-A1-image-processing.rkt
@@ -13,3 +13,33 @@
                  (square 10 "solid" "aqua"))
          (beside (square 10 "solid" "gold")
                  (square 10 "solid" "magenta"))))
+
+; Prozedur "rows-worker" soll
+; - eine natürliche Zahl w und eine Liste xs akzeptieren
+; - übergebene Liste xs soll in eine Liste von Listen xss umgewandelt werden
+; - jede innere Liste soll w Elemnte enthalten
+; - letzte inner Lite kann weniger als w Elemente enthalten
+
+; Prozedur "rows" soll auf "rows-worker" zugreifen -> endrekursiv
+
+; schreiben einer Prozedur "flatten", die
+; - eine Liste von Listen xss akzeptiert
+; - eine Liste von Listen in eine Liste wandelt, die alle Elemente aus xss  enthält
+
+; schreiben einer Prozedur "transpose", die
+; - eine Liste von Listen xss akzeptiert
+; - die übergebene Liste "transponiert"
+;   (bei 2-dimensionalen Matrix: Zeilen zu Spalten umwandelt)
+; - innere Listen jeweils gleich lang
+
+;================================================================================================
+
+; Zugriff auf die Liste der Bildpunkte (Pixel) eines Bildes:  
+; (: image->color-list (image -> (list-of rgb-color)))  
+; (: color-list->bitmap ((list-of rgb-color) natural natural -> image))
+
+; Prozedur "vert-mirror" soll Bild vertikal spiegeln
+
+; Prozedur "horiz-mirror" soll Bild horizontal spiegeln
+
+; Prozedur "rotate-90-left" soll ein Bild um 90° gegen Uhrzeigersinn rotieren
