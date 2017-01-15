c69114c60b5455b5a772c2f14e6a0856d3a6762b
Kim <Kim@Kims-MacBook-Pro.local>
2017-01-15 22:05:59 +0100

    Add record for office

--- Blatt10-A1-human-resource-machine.rkt
+++ Blatt10-A1-human-resource-machine.rkt
@@ -2,3 +2,57 @@
 ;; über die Sprachebene dieser Datei in einer Form, die DrRacket verarbeiten kann.
 #reader(lib "DMdA-vanilla-reader.ss" "deinprogramm")((modname Blatt10-A1-human-resource-machine) (read-case-sensitive #f) (teachpacks ((lib "universe.rkt" "teachpack" "deinprogramm") (lib "image2.rkt" "teachpack" "deinprogramm"))) (deinprogramm-settings #(#f write repeating-decimal #f #t none explicit #f ((lib "universe.rkt" "teachpack" "deinprogramm") (lib "image2.rkt" "teachpack" "deinprogramm")))))
 ; Aufgabe: Human Resource Machine
+
+; --------------------------------------------------------------------------------------------------------------
+; The office
+; --------------------------------------------------------------------------------------------------------------
+; A post office consists of
+; - an inbox for incomming packages
+; - an outbox for outgoing packages
+; - a floor of fixed size to work on. Each slot contains either a package or nothing
+; - a worker who can carry a single package (or nothing)
+; - a list of instructions, telling the worker what to do step by step
+; - an instruction pointer, telling the worker what to do next. It is either an absolute
+;   position on the instructions board, starting from 0, or #f for "finish work!"
+; - a time clock which records the total time of work (in number of performed instructions)
+(define-record-procedures office
+  make-office
+  office?
+  (inbox
+   outbox
+   floor-slots
+   worker
+   instruction-list
+   ip
+   time-clock))
+(: make-office ((list-of package)
+                (list-of package)
+                (list-of (maybe-of package))
+                (maybe-of package)
+                (list-of (mixed instruction string))
+                (maybe-of natural)
+                natural
+                -> office))
+(: office? (any -> boolean))
+(: inbox (office -> (list-of package)))
+(: outbox (office -> (list-of package)))
+(: floor-slots (office -> (list-of (maybe-of package))))
+(: worker (office -> (maybe-of package)))
+(: instruction-list (office -> (list-of (mixed instruction string))))
+(: ip (office -> (maybe-of natural)))
+(: time-clock (office -> natural))
+
+; A package contains eigther a number or a character
+(define package (signature (mixed integer
+                                  character)))
+
+; A character is an upper case letter of the alphabet between "A" and "Z" 
+(define character (signature (predicate
+                              (lambda (c)
+                                (and (string? c)
+                                     (string<=? "A" c "Z"))))))
+; A (maybe-of ⟨t⟩) is either an element of signature ⟨t⟩ or empty (#f)
+(: maybe-of (signature -> signature))
+(define maybe-of
+  (lambda (t)
+    (signature (mixed t (one-of #f)))))
