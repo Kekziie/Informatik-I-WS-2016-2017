9475061282acce489c121d871e9730e2d60374e1
Kim <Kim@Kims-MacBook-Pro.local>
2017-01-15 20:14:44 +0100

    add two helper procedures

--- Blatt08-A1-image-processing.rkt
+++ Blatt08-A1-image-processing.rkt
@@ -14,6 +14,52 @@
          (beside (square 10 "solid" "gold")
                  (square 10 "solid" "magenta"))))
 
+; Hilfsprozeduren für "rows-worker":
+; - take
+; - drop
+
+; (take w xs):
+; - liefert die ersten w Elemente der Liste xs zurück
+; - hat xs nur m < w Elemente, liefere diese m Elemente zurück
+(: take (natural (list-of %a) -> (list-of %a)))
+
+(check-expect (take 0 empty) empty)
+(check-expect (take 0 (list 1)) empty)
+(check-expect (take 2 (list 1)) (list 1))
+(check-expect (take 2 (list 1 2)) (list 1 2))
+(check-expect (take 2 (list 1 2 3)) (list 1 2))
+(check-expect (take 3 (list 1 2 3 4 5 6 7 8 9)) (list 1 2 3))
+
+(define take
+  (lambda (w xs)
+    (cond
+      ((empty? xs) empty)
+      ((= 0 w) empty)
+      ((pair? xs) (if (<= (length xs) w)
+                      xs
+                      (make-pair (first xs)
+                                 (take (- w 1) (rest xs))))))))  
+
+; (drop w xs)
+; - verwirft die ersten w Elemente der Liste xs und gibt den Rest zurück
+; - hat xs nur m < w Elemente, liefere die leere Liste zurück
+(: drop (natural (list-of %a) -> (list-of %a)))
+
+(check-expect (drop 0 empty) empty)
+(check-expect (drop 0 (list 3)) (list 3))
+(check-expect (drop 1 (list 1)) empty)
+(check-expect (drop 2 (list 1 2 3)) (list 3))
+(check-expect (drop 3 (list 1 2 3 4 5 6 7 8 9)) (list 4 5 6 7 8 9))
+
+(define drop
+  (lambda (w xs)
+    (cond
+      ((empty? xs) empty)
+      ((= w 0) xs)
+      ((pair? xs) (if (<= (length xs) w)
+                       empty
+                      (drop (- w 1) (rest xs)))))))
+
 ; Prozedur "rows-worker" soll
 ; - eine natürliche Zahl w und eine Liste xs akzeptieren
 ; - übergebene Liste xs soll in eine Liste von Listen xss umgewandelt werden
