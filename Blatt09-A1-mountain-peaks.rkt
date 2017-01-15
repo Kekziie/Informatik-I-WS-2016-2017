b57952da2a67f52a9daad4e5a31f5e760cba30dc
Kim <Kim@Kims-MacBook-Pro.local>
2017-01-15 21:58:34 +0100

    New file

---
+++ Blatt09-A1-mountain-peaks.rkt
@@ -0,0 +1,4 @@
+;; Die ersten drei Zeilen dieser Datei wurden von DrRacket eingefügt. Sie enthalten Metadaten
+;; über die Sprachebene dieser Datei in einer Form, die DrRacket verarbeiten kann.
+#reader(lib "DMdA-vanilla-reader.ss" "deinprogramm")((modname Blatt09-A1-mountain-peaks) (read-case-sensitive #f) (teachpacks ((lib "universe.rkt" "teachpack" "deinprogramm") (lib "image2.rkt" "teachpack" "deinprogramm"))) (deinprogramm-settings #(#f write repeating-decimal #f #t none explicit #f ((lib "universe.rkt" "teachpack" "deinprogramm") (lib "image2.rkt" "teachpack" "deinprogramm")))))
+; Aufgabe 1
