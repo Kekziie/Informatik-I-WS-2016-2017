c245f34b065220887e2d00b1fcdd5e18f2834b37
Kim <Kim@Kims-MacBook-Pro.local>
2017-01-15 21:47:49 +0100

    new file

---
+++ Blatt09-A4-zipWith.rkt
@@ -0,0 +1,8 @@
+;; Die ersten drei Zeilen dieser Datei wurden von DrRacket eingefügt. Sie enthalten Metadaten
+;; über die Sprachebene dieser Datei in einer Form, die DrRacket verarbeiten kann.
+#reader(lib "DMdA-vanilla-reader.ss" "deinprogramm")((modname Blatt09-A4-zipWith) (read-case-sensitive #f) (teachpacks ((lib "universe.rkt" "teachpack" "deinprogramm") (lib "image2.rkt" "teachpack" "deinprogramm"))) (deinprogramm-settings #(#f write repeating-decimal #f #t none explicit #f ((lib "universe.rkt" "teachpack" "deinprogramm") (lib "image2.rkt" "teachpack" "deinprogramm")))))
+; Aufgabe 4
+; H.O.P. "zipWith" soll:
+;  - eine Funktion f und 2 Listen xs und ys konsumieren
+;  - kombiniert Elemente der Listen xs und ys an der gleichen Position mittels funktion f
+;  - überstehende Elemente verwerfen
