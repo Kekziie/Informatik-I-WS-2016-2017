959fb34e9497eaf5ec0f9c041864b4c72a3b9bfd
Kim Thuong Ngo <kimthuongngo3@gmail.com>
2017-01-15 10:39:14 +0100

    add task

--- Blatt08-A1-image-processing.rkt
+++ Blatt08-A1-image-processing.rkt
@@ -1,3 +1,8 @@
 ;; Die ersten drei Zeilen dieser Datei wurden von DrRacket eingefügt. Sie enthalten Metadaten
 ;; über die Sprachebene dieser Datei in einer Form, die DrRacket verarbeiten kann.
 #reader(lib "DMdA-vanilla-reader.ss" "deinprogramm")((modname Blatt08-A1-image-processing) (read-case-sensitive #f) (teachpacks ((lib "image2.rkt" "teachpack" "deinprogramm") (lib "universe.rkt" "teachpack" "deinprogramm"))) (deinprogramm-settings #(#f write repeating-decimal #f #t none explicit #f ((lib "image2.rkt" "teachpack" "deinprogramm") (lib "universe.rkt" "teachpack" "deinprogramm")))))
+; Aufgabe
+; arbeiten mit Bildern:
+; - vertikal spiegeln
+; - horizontal spiegeln
+; - 90° gegen Uhrzeigersinn rotieren
