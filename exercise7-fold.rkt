1a175312a0c7bad34905db90c0e35ac7e7ea9209
Kim <Kim@Kims-MacBook-Pro.local>
2017-01-17 18:25:38 +0100

    New file

---
+++ exercise7-fold.rkt
@@ -0,0 +1,4 @@
+;; Die ersten drei Zeilen dieser Datei wurden von DrRacket eingefügt. Sie enthalten Metadaten
+;; über die Sprachebene dieser Datei in einer Form, die DrRacket verarbeiten kann.
+#reader(lib "DMdA-beginner-reader.ss" "deinprogramm")((modname exercise7-fold) (read-case-sensitive #f) (teachpacks ((lib "universe.rkt" "teachpack" "deinprogramm") (lib "image2.rkt" "teachpack" "deinprogramm"))) (deinprogramm-settings #(#f write repeating-decimal #f #t none explicit #f ((lib "universe.rkt" "teachpack" "deinprogramm") (lib "image2.rkt" "teachpack" "deinprogramm")))))
+; Übung 7 fold
