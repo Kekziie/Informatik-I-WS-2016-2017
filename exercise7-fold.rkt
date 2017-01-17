861e0a8b7e8ab312fe62776259c3044c0975f3cc
Kim <Kim@Kims-MacBook-Pro.local>
2017-01-17 18:31:36 +0100

    add procedure folder

--- exercise7-fold.rkt
+++ exercise7-fold.rkt
@@ -1,4 +1,15 @@
 ;; Die ersten drei Zeilen dieser Datei wurden von DrRacket eingefügt. Sie enthalten Metadaten
 ;; über die Sprachebene dieser Datei in einer Form, die DrRacket verarbeiten kann.
-#reader(lib "DMdA-beginner-reader.ss" "deinprogramm")((modname exercise7-fold) (read-case-sensitive #f) (teachpacks ((lib "universe.rkt" "teachpack" "deinprogramm") (lib "image2.rkt" "teachpack" "deinprogramm"))) (deinprogramm-settings #(#f write repeating-decimal #f #t none explicit #f ((lib "universe.rkt" "teachpack" "deinprogramm") (lib "image2.rkt" "teachpack" "deinprogramm")))))
-; Übung 7 fold
+#reader(lib "DMdA-vanilla-reader.ss" "deinprogramm")((modname exercise7-fold) (read-case-sensitive #f) (teachpacks ((lib "image2.rkt" "teachpack" "deinprogramm") (lib "universe.rkt" "teachpack" "deinprogramm"))) (deinprogramm-settings #(#f write repeating-decimal #f #t none explicit #f ((lib "image2.rkt" "teachpack" "deinprogramm") (lib "universe.rkt" "teachpack" "deinprogramm")))))
+; Übung 7 fold "Listenfaltung"
+
+; i) foldr(ight)
+; Listenfaltung rechts
+(: foldr (%b (%a %b -> %b) (list-of %a) -> %b))
+
+(define foldr
+  (lambda (z c xs)
+    (cond
+      ((empty? xs) z)
+      ((pair? xs) (c (first xs)
+                     (foldr z c (rest xs)))))))
