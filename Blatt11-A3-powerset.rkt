db2a28c434c9b41f9cd961d8f63845706f480cf2
move file <Kim@Kims-MacBook-Pro.local>
2017-01-18 19:56:01 +0100

    add test and procedure

--- Blatt11-A3-powerset.rkt
+++ Blatt11-A3-powerset.rkt
@@ -8,4 +8,21 @@
 ; z.B. P({1,2,3}) = {{1,2,3},{1,2},{2,3},{1,3},{1},{2},{3},Ø}
 
 ; Funktion "powerset" soll Potenzmenge einer Menge von beliebigen Elementen berechnen
+; akzeptiert eine beliebige Liste xs und erstellt die Potenzmenge als Liste von Listen
 (: powerset ((list-of %a) -> (list-of (list-of %a))))
+
+(check-expect (powerset (list 1 2 3)) (list (list Ø) (list 1 2 3) (list 1 2) (list 2 3) (list 1 3) (list 1) (list 2) (list 3)))
+(check-expect (powerset (list #t #f)) (list (list Ø) (list #t #f) (list #t) (list #f)))
+(check-expect (powerset empty) (list (list Ø)))
+
+(define powerset
+  (lambda (xs)
+    (cond
+      ((empty? xs) (make-pair (list Ø)
+                              empty))
+      (else (make-pair (list Ø)
+                       (make-pair xs
+                                  (map (lambda (x)
+                                         (make-pair (first xs) x))
+                                       (powerset (rest xs)))))))))
+      
