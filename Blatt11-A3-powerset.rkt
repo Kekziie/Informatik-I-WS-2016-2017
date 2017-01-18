d667ee185609f2637a697f4f0a3ae14f0bb26b3a
move file <Kim@Kims-MacBook-Pro.local>
2017-01-18 18:55:35 +0100

    new file

---
+++ Blatt11-A3-powerset.rkt
@@ -0,0 +1,4 @@
+;; Die ersten drei Zeilen dieser Datei wurden von DrRacket eingefügt. Sie enthalten Metadaten
+;; über die Sprachebene dieser Datei in einer Form, die DrRacket verarbeiten kann.
+#reader(lib "DMdA-vanilla-reader.ss" "deinprogramm")((modname Blatt11-A3-powerset) (read-case-sensitive #f) (teachpacks ()) (deinprogramm-settings #(#f write repeating-decimal #f #t none explicit #f ())))
+; Aufgabe 3
