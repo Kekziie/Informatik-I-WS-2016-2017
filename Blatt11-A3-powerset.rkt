c2f20868fc910c47b7ee79c04f74fa1c32bc5188
Kim <Kim@Kims-MacBook-Pro.local>
2017-01-19 11:24:27 +0100

    Add procedure

--- Blatt11-A3-powerset.rkt
+++ Blatt11-A3-powerset.rkt
@@ -42,11 +42,15 @@
 
 ; Funktion "powerset" soll Potenzmenge einer Menge von beliebigen Elementen berechnen
 ; akzeptiert eine beliebige Liste xs und erstellt die Potenzmenge als Liste von Listen
-;(: powerset ((list-of %a) -> (list-of (list-of %a))))
-;
-;(check-expect (powerset (list 1 2 3)) (list (list "Ø") (list 1 2 3) (list 1 2) (list 2 3) (list 1 3) (list 1) (list 2) (list 3)))
-;(check-expect (powerset (list #t #f)) (list (list "Ø") (list #t #f) (list #t) (list #f)))
-;(check-expect (powerset empty) (list (list "Ø")))
+(: powerset ((list-of %a) -> (list-of (list-of %a))))
+
+(check-expect (powerset (list 1 2 3)) (list (list "Ø") (list 1 2 3) (list 1 2) (list 2 3) (list 1 3) (list 1) (list 2) (list 3)))
+(check-expect (powerset (list #t #f)) (list (list "Ø") (list #t #f) (list #t) (list #f)))
+(check-expect (powerset empty) (list (list "Ø")))
+
+(define powerset
+  (lambda (xs)
+    (change (powerset-worker xs))))
 
 
       
