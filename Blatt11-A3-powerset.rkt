1f8807cf04698656595a4429cb3493908c997bd8
Kim <Kim@Kims-MacBook-Pro.local>
2017-01-19 11:11:54 +0100

    Add helper procedure

--- Blatt11-A3-powerset.rkt
+++ Blatt11-A3-powerset.rkt
@@ -7,20 +7,25 @@
 ; P(S) enthält dabei immer die leere Menge Ø sowie S selbst
 ; z.B. P({1,2,3}) = {{1,2,3},{1,2},{2,3},{1,3},{1},{2},{3},Ø}
 
-; Funktion "powerset" soll Potenzmenge einer Menge von beliebigen Elementen berechnen
-; akzeptiert eine beliebige Liste xs und erstellt die Potenzmenge als Liste von Listen
-(: powerset ((list-of %a) -> (list-of (list-of %a))))
-
-(check-expect (powerset (list 1 2 3)) (list (list "Ø") (list 1 2 3) (list 1 2) (list 2 3) (list 1 3) (list 1) (list 2) (list 3)))
-(check-expect (powerset (list #t #f)) (list (list "Ø") (list #t #f) (list #t) (list #f)))
-(check-expect (powerset empty) (list (list "Ø")))
-
-(define powerset
+; Hilfsprozedur powerset-worker
+; erstellt alle mögliche Kombinationen von der Liste xs
+(define powerset-worker
   (lambda (xs)
     (cond
-      ((empty? xs) (make-pair (list "Ø")
+      ((empty? xs) (make-pair empty
                               empty))
-      (else (make-pair (list "Ø")
-                       (make-pair xs
-                                  ...))))))
+      (else  (append (powerset (rest xs))
+                          (map (lambda (x)
+                                 (make-pair (first xs) x))
+                               (powerset (rest xs))))))))
+
+; Funktion "powerset" soll Potenzmenge einer Menge von beliebigen Elementen berechnen
+; akzeptiert eine beliebige Liste xs und erstellt die Potenzmenge als Liste von Listen
+;(: powerset ((list-of %a) -> (list-of (list-of %a))))
+;
+;(check-expect (powerset (list 1 2 3)) (list (list "Ø") (list 1 2 3) (list 1 2) (list 2 3) (list 1 3) (list 1) (list 2) (list 3)))
+;(check-expect (powerset (list #t #f)) (list (list "Ø") (list #t #f) (list #t) (list #f)))
+;(check-expect (powerset empty) (list (list "Ø")))
+
+
       
