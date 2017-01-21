de71ca317d97917c947bfc896f8fd033e1047934
Kim <Kim@Kims-MacBook-Pro.local>
2017-01-21 12:03:59 +0100

    Add task

--- Blatt12-A1-bintree.rkt
+++ Blatt12-A1-bintree.rkt
@@ -2,3 +2,11 @@
 ;; über die Sprachebene dieser Datei in einer Form, die DrRacket verarbeiten kann.
 #reader(lib "DMdA-vanilla-reader.ss" "deinprogramm")((modname Blatt12-A1-bintree) (read-case-sensitive #f) (teachpacks ()) (deinprogramm-settings #(#f write repeating-decimal #f #t none explicit #f ())))
 ; Aufgabe 1
+
+; Prozedur btree-min
+; ermittelt minimalste Markierung eines Binärbaumes
+(: btree-min ((btree-of real) -> real))
+
+; Prozedur btree-max
+; ermittelt maximalste Markierung eines Binärbaumes
+(: btree-max ((btree-of real) -> real))
