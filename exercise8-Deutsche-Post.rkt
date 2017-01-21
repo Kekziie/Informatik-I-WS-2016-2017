b6da1492e428cf5e430b229864cfcc765e518632
Kim <Kim@Kims-MacBook-Pro.local>
2017-01-21 13:11:30 +0100

    add record for size

--- exercise8-Deutsche-Post.rkt
+++ exercise8-Deutsche-Post.rkt
@@ -3,6 +3,22 @@
 #reader(lib "DMdA-vanilla-reader.ss" "deinprogramm")((modname exercise8-Deutsche-Post) (read-case-sensitive #f) (teachpacks ()) (deinprogramm-settings #(#f write repeating-decimal #f #t none explicit #f ())))
 ; Übung 8 Deutsche Post 2107
 
+; ein Maß (size) besteht aus:
+; - Länge (length)
+; - Breite (width)
+; - Höhe (height)
+(: make-size (real real real -> size))
+(: size-length (size -> real))
+(: size-width (size -> real))
+(: size-height (size -> real))
+
+(define-record-procedures size
+  make-size
+  size?
+  (size-length
+   size-width
+   size-height))
+
 ; ein Brief (letter) besteht aus:
 ; - Name (name)
 ; - Gewicht (weight)
