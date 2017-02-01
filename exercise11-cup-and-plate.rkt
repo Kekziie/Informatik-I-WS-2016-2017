5c2b8cc820d68a66a15fe8c49ad91636c9996271
Kim <Kim@Kims-MBP.fritz.box>
2017-02-01 15:16:59 +0100

    Add records

--- exercise11-cup-and-plate.rkt
+++ exercise11-cup-and-plate.rkt
@@ -2,3 +2,34 @@
 ;; über die Sprachebene dieser Datei in einer Form, die DrRacket verarbeiten kann.
 #reader(lib "DMdA-vanilla-reader.ss" "deinprogramm")((modname exercise11-cup-and-plate) (read-case-sensitive #f) (teachpacks ()) (deinprogramm-settings #(#f write repeating-decimal #f #t none explicit #f ())))
 ; Übung 11 Tasse und Teller
+
+; Eine Tasse (cup) besteht aus:
+; - Volumen (vol)
+; - Farbe (col)
+
+(define-record-procedures cup
+  make-cup
+  cup?
+  (cup-vol
+   cup-col))
+
+(: make-cup (real string -> cup))
+(: cup-vol (cup -> real))
+(: cup-col (cup -> string))
+(: cup? (any -> boolean))
+
+; Ein Teller (plate) besteht aus:
+; - Durchmesser (dia)
+; - Farbe (col)
+
+(define-record-procedures plate
+  make-plate
+  plate?
+  (plate-dia
+   plate-col))
+
+(: make-col (real string -> plate))
+(: plate-dia (plate -> real))
+(: plate-col (plate -> string))
+(: plate? (any -> boolean))
+
