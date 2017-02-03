00e38d60db5374bdfd0aa4e140eb84d17ad8b819
Kim <Kim@Kims-MacBook-Pro.local>
2017-02-03 12:43:46 +0100

    New file

---
+++ example3-btree.rkt
@@ -0,0 +1,4 @@
+;; Die ersten drei Zeilen dieser Datei wurden von DrRacket eingefügt. Sie enthalten Metadaten
+;; über die Sprachebene dieser Datei in einer Form, die DrRacket verarbeiten kann.
+#reader(lib "DMdA-vanilla-reader.ss" "deinprogramm")((modname example3-btree) (read-case-sensitive #f) (teachpacks ()) (deinprogramm-settings #(#f write repeating-decimal #f #t none explicit #f ())))
+; Beispiele 3 - Binärbäume
