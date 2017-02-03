f39740da5e8c34e925adf33ac15db77108962f52
Kim <Kim@Kims-MacBook-Pro.local>
2017-02-03 12:45:37 +0100

    add definition for btrees

--- example3-btree.rkt
+++ example3-btree.rkt
@@ -2,3 +2,49 @@
 ;; über die Sprachebene dieser Datei in einer Form, die DrRacket verarbeiten kann.
 #reader(lib "DMdA-vanilla-reader.ss" "deinprogramm")((modname example3-btree) (read-case-sensitive #f) (teachpacks ()) (deinprogramm-settings #(#f write repeating-decimal #f #t none explicit #f ())))
 ; Beispiele 3 - Binärbäume
+
+;----------------------------------------------------------------------------
+; Definition: Binärbaum
+;----------------------------------------------------------------------------
+
+; Ein Knoten (node) besitzt
+; - einen linken Zweig (left-branch),
+; - eine Markierung (label) und
+; - einen rechten Zweig (right-branch)
+(: make-node (%a %b %c -> (node-of %a %b %c)))
+(: node-left-branch  ((node-of %a %b %c) -> %a))
+(: node-label        ((node-of %a %b %c) -> %b))
+(: node-right-branch ((node-of %a %b %c) -> %c))
+(define-record-procedures-parametric node node-of
+  make-node
+  node?
+  (node-left-branch
+   node-label
+   node-right-branch))
+
+; Der leere Baum (the-empty-tree) besitzt keine weiteren Eigenschaften
+(: make-empty-tree (-> the-empty-tree))
+(define-record-procedures the-empty-tree
+  make-empty-tree
+  empty-tree?
+  ())
+
+; Der leere Baum 
+(: empty-tree the-empty-tree)
+(define empty-tree (make-empty-tree))
+
+; Signatur für Binärbäume (btree-of t) mit Markierungen der Signatur t
+; (im linken/rechten Zweig jedes Knotens findet sich jeweils wieder
+; ein Binärbaum)
+(define btree-of
+  (lambda (t)
+    (signature (mixed the-empty-tree
+                      (node-of (btree-of t) t (btree-of t))))))
+
+; Konstruiere ein Blatt mit Markierung x
+(: make-leaf (%a -> (btree-of %a)))
+(define make-leaf
+  (lambda (x)
+    (make-node empty-tree
+               x
+               empty-tree)))
